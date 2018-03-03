1798969bcd01556990d3efff6780a43bdcbd7c8d
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-03-03 15:19:55 +0600

    Install nodemon

--- Express-Demo/index.js
+++ Express-Demo/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World Mitul');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([50,1,2,3
-    ]);
-  });
+  res.send([50, 1, 2, 3]);
+});
 
 app.listen(3000, () => console.log('Listening port 3000 ...'));
