4638d73077ba8e953022dec6dabbc6dead8a0339
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-03-03 21:58:47 +0600

    Handling HTTP GET Requests

--- Express-Demo/index.js
+++ Express-Demo/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express();
+var courses = [
+  { id: 1, name: 'Course1' },
+  { id: 2, name: 'Course2' },
+  { id: 3, name: 'Course3' },
+  { id: 4, name: 'Course4' },
+];
 app.get('/', (req, res) => {
   res.send('Hello World Mitul');
 });
 
 app.get('/api/courses', (req, res) => {
-  res.send([50, 1, 2, 3]);
+  res.send(courses);
 });
 app.get('/api/courses/:id', (req, res) => {
-  res.send(req.params.id);
+  const course = courses.find(s => s.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send('The course with the given ID was not found');
+  res.send(course);
 });
 
 const port = process.env.PORT || 3000;
