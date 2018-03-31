93c3d0ed2e443483dcfb945433bc256c96ea0757
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-04-01 00:35:55 +0600

    Callbacks

--- Async-Demo/index.js
+++ Async-Demo/index.js
@@ -1,11 +1,19 @@
 console.log('Before');
-const user = getUser(1);
-console.log(user);
+const user = getUser(1, user => {
+  getRepositories(user.gitHubUsername, repos => {
+    console.log('Repos:', repos);
+  });
+});
 console.log('After');
-
-function getUser(id) {
+function getUser(id, callback) {
   setTimeout(() => {
     console.log('Reading a user from database', id);
-    return { id: id, gitHubUsername: 'mitul' };
+    callback({ id: id, gitHubUsername: 'mitul' });
+  }, 2000);
+}
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log('Reading a user repo from database', username);
+    callback(['repo1', 'repo2', 'repo3']);
   }, 2000);
 }
