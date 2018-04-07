04514e5a9676f445279be6d3db9708b6e6aa6ef6
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-04-07 17:07:33 +0600

    models

--- Mongo-Demo/index.js
+++ Mongo-Demo/index.js
@@ -1,6 +1,22 @@
-const mongooes = require('mongoose');
+const mongoose = require('mongoose');
 
-mongooes
+mongoose
   .connect('mongodb://localhost/playground')
   .then(() => console.log('Connected to mongodb ..'))
   .catch(err => console.log('Could not connect to MongoDb ..', err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+const Course = mongoose.model('Course', courseSchema);
+const course = new Course({
+  name: 'Node JS',
+  author: 'Mosh',
+  tags: ['node', 'backend'],
+  isPublished: true,
+});
