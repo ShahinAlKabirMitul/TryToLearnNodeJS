0ad80d5d6059b473f47d5871072b374a0df536e6
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-04-07 17:17:09 +0600

    Saving document

--- Mongo-Demo/index.js
+++ Mongo-Demo/index.js
@@ -14,9 +14,15 @@ const courseSchema = new mongoose.Schema({
 });
 
 const Course = mongoose.model('Course', courseSchema);
-const course = new Course({
-  name: 'Node JS',
-  author: 'Mosh',
-  tags: ['node', 'backend'],
-  isPublished: true,
-});
+
+async function createCourse() {
+  const course = new Course({
+    name: 'Angular Course',
+    author: 'Mosh',
+    tags: ['angular', 'fontend'],
+    isPublished: true,
+  });
+  const result = await course.save();
+  console.log(result);
+}
+createCourse();
