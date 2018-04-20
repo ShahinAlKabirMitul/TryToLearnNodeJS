ae452f4d8c3d0701f6ff52058e3f40f744bfb2bd
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-04-20 12:23:26 +0600

    Exercise 2

---
+++ Mongo-Demo/solution2.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+mongoose
+  .connect('mongodb://localhost/mongo-exercises')
+  .then(() => console.log('Connected to mongodb ..'))
+  .catch(err => console.log('Could not connect to mongo-exercises ..', err));
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+  price: Number,
+});
+const Course = mongoose.model('Course', courseSchema);
+async function getCourses() {
+  return await Course.find()
+    // Starts with
+    .find({ isPublished: true, tags: { $in: ['backend', 'frontend'] } })
+    .limit(50)
+    .sort({ price: -1 })
+    .select({ name: 1, author: 1, price: 1 });
+}
+async function run() {
+  const courses = await getCourses();
+  console.log(courses);
+}
+run();
