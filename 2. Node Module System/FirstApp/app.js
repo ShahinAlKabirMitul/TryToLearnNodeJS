0cf33fc58c098ba94621969a0c2cae96f66b284d
shahinalkabirmitul <mitul.cse@gmail.com>
2018-06-22 13:33:24 +0600

    OS Module

--- 2. Node Module System/FirstApp/app.js
+++ 2. Node Module System/FirstApp/app.js
@@ -1,3 +1,8 @@
-const path = require('path');
-const pathObj = path.parse(__filename);
-console.log(pathObj);
+
+const os = require('os');
+
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
+console.log(`Total Memory ${totalMemory}`);
+console.log(`Total Free Memory ${freeMemory}`);
+
