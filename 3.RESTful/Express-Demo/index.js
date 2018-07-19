632540e8b709022ebd4c0b4223dab84d90abf83c
shahinalkabirmitul <mitul.cse@gmail.com>
2018-07-19 21:57:16 +0600

    Handleing http put request

--- 3.RESTful/Express-Demo/index.js
+++ 3.RESTful/Express-Demo/index.js
@@ -24,25 +24,41 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+
+  courses.push(course);
+  res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+  course.name = req.body;
+  res.send(course);
+});
+
+function validateCourse(course) {
   const schema = {
     name: joi
       .string()
       .min(3)
       .required(),
   };
-  const result = joi.validate(req.body, schema);
-
-  console.log(result);
-
-  if (result.error) res.status(400).send(result.error);
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name,
-  };
-  console.log(course);
-  courses.push(course);
-  res.send(course);
-});
+  return joi.validate(course, schema);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listing port ${port}`));
