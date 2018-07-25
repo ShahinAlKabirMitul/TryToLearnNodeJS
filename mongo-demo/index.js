5d0fb32bc17b4ab2808a32a9c882359c276602dc
shahinalkabirmitul <mitul.cse@gmail.com>
2018-07-25 06:54:12 +0600

    Mongoose Schemas

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -3,3 +3,11 @@ mongoose
   .connect('mongodb://localhost/playground')
   .then(() => console.log('Connedted to Mongo db database '))
   .catch(err => console.log('Could not connect to Mongo db ....', err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
